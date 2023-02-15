5e50c99cc5bce2ef9bcd498d7e3bf186c41d624f
Hafimuthasir <muthasirhafi@gmail.com>
2023-02-15 18:18:01 +0530

    baseurl update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const apibaseUrl = 'http://localhost:8000/api/';
-export const adminApibaseUrl = 'http://localhost:8000/api2/';
+export const apibaseUrl = 'https://grapicscard.ga/api/';
+export const adminApibaseUrl = 'https://grapicscard.ga/api2/';
 export const imagebaseUrl = 'C:/Users/AKAM/Desktop/React/week2/djangoproject/reactapp/src'
