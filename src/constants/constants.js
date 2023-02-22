8e46489f983fa06098bc163aa79b0883e17cd9cc
Hafimuthasir <muthasirhafi@gmail.com>
2023-02-22 16:52:57 +0530

    m

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const apibaseUrl = 'http://localhost:8000/api/';
-// export const apibaseUrl = 'https://grapicscard.ga/api/';
+// export const apibaseUrl = 'http://localhost:8000/api/';
+export const apibaseUrl = 'https://grapicscard.ga/api/';
 export const adminApibaseUrl = 'https://grapicscard.ga/api2/';
 export const imagebaseUrl = 'C:/Users/AKAM/Desktop/React/week2/djangoproject/reactapp/src'
